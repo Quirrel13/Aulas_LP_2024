cb712be95d7ae1f8f98c55d491f0197a95b03ebd
Q13 <joaosferla@gmail.com>
2024-11-20 18:26:01 -0300

    a

---
+++ 18 - Trabalho arquivo/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "../Biblio/q13.h"
+
+int main(){
+
+    int op;
+
+    printf("---------------------------------------\n");
+    printf("--- Gerenciamento - Lanchonete IFPR ---\n");
+    printf("---------------------------------------\n");
+    printf("1) Comprar Produtos -------------------\n");
+    printf("2) Vender Produtos --------------------\n");
+    printf("3) Total de Produtos Comprados --------\n");
+    printf("4) Total de Produtos Vendidos ---------\n");
+    printf("5) Total Gasto em Compras -------------\n");
+    printf("6) Total Recebido em Vendas -----------\n");
+    printf("7) Lucro Total ------------------------\n");
+    printf("0) Sair -------------------------------\n");
+    printf("---------------------------------------\n");
+
+    return 0;
+
+}
